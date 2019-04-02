c37799252d49356aa3c492bcf088c752e05e6415
Hung <hungbeo1503hnqn@gmail.com>
2019-04-02 09:27:58 +0100

    modified main.c for recording

--- main.c
+++ main.c
@@ -2,14 +2,32 @@
 #include <time.h>
 #include "screen.h"
 #include <stdio.h>
+#include "sound.h"
+
 
 int main(){
-	int dec[COL];		// 80-pieces of sound decibels
-	srand(time(NULL));
-	for(int i=0; i<COL; i++) dec[i] = rand()%70+30; 
-	clearScreen();
-	setColors(RED, bg(WHITE));
-	barChart(dec);
+	FILE *f;
+	short sd[80000];
+	for(;;){
+		system(RCMD);
+		f = fopen("test.wav", "r");
+		if(f == NULL){
+			printf("Cannot open the file");
+			return 1;
+		}
+//	int dec[COL];		// 80-pieces of sound decibels
+//	srand(time(NULL));
+//	for(int i=0; i<COL; i++) dec[i] = rand()%70+30; 
+		clearScreen();
+		setColors(RED, bg(WHITE));
+//	barChart(dec);
+		struct WAVHDR hdr;
+		fread(&hdr, sizeof(hdr), 1, f);		// read WAV header
+		fread(&sd, sizeof(sd), 1, f);		// read WAV data
+		fclose(f);
+		displayWAVHDR(hdr);
+		// displayWAVDATA();
+	}
 	resetColors();
 	getchar();
 }
---
+++ sound.h
@@ -0,0 +1,28 @@
+// constants definitions
+#define RATE 16000
+#define DUR 5
+#define RCMD "arecord -r16000 -c1 -f S16_LE -d5 test.wav"
+// data structures
+struct WAVHDR{
+	char ChunkID[4];		// must be "RIFF"
+	int ChunkSize;
+	char Format[4];			// must be "WAVE"
+
+	char Subchunk1ID[4];	// must be "fmt"
+	int Subchunk1Size;		// should be 16
+	short AudioFormat;		// should be 1
+	short NumChannels; 		// should be 1
+	int SampleRate;			// we will value 16000
+	int ByteRate;			// calculated by SR, NS, B/S
+	short BlockAlign;		// calculated
+	short BitsPerSample;	// should be 16
+
+	char Subchunk2ID[4];	// must be "data"
+	int Subchunk2Size;		// calculated
+};
+
+// function declaration
+void displayWAVHDR(struct WAVHDR);
+void showID(char *, char *);
+void displayWAVDATA(short []);
+
